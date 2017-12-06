520bc9ddfb6a4d78e85e0a29871838bb7fb66c80
ios2 <chenyihang520025@126.com>
2017-12-06 17:55:19 +0800

    no message

--- cmigu/blog/adminx.py
+++ cmigu/blog/adminx.py
@@ -22,16 +22,16 @@ class AdminSettings(object):
     menu_style = 'default'
 
     # 菜单设置
-    def get_site_menu(self):
-        return (
-            {'title': '博客管理', 'perm': self.get_model_perm(Page, 'change'), 'menus': (
-                {'title': '所有页面', 'icon': 'fa fa-vimeo-square'
-                    , 'url': self.get_model_url(Page, 'changelist')},
-                {'title': '分类目录', 'icon': 'fa fa-vimeo-square'
-                    , 'url': self.get_model_url(Category, 'changelist')},
-            )},
-        )
-xadmin.site.register(xviews.CommAdminView, AdminSettings)
+    # def get_site_menu(self):
+    #     return (
+    #         {'title': '博客管理', 'perm': self.get_model_perm(Page, 'change'), 'menus': (
+    #             {'title': '所有页面', 'icon': 'fa fa-vimeo-square'
+    #                 , 'url': self.get_model_url(Page, 'changelist')},
+    #             {'title': '分类目录', 'icon': 'fa fa-vimeo-square'
+    #                 , 'url': self.get_model_url(Category, 'changelist')},
+    #         )},
+    #     )
+xadmin.site.register(xviews.CommAdminView)
 
 xadmin.site.register(Page)
 xadmin.site.register(Category)
